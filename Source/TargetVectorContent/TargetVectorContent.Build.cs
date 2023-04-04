6558ec0a93bc3ba1061ff0774703b8c9df5323bf
colorindarkness <urielballinas@gmail.com>
2023-04-04 01:22:37 -0700

    Fix build settings and gitattributes

--- Source/TargetVectorContent/TargetVectorContent.Build.cs
+++ Source/TargetVectorContent/TargetVectorContent.Build.cs
@@ -25,7 +25,7 @@ public class TargetVectorContent : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core", "TargetVectorCode", "TargetVectorCommonUI", "TargetVectorEOS",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
